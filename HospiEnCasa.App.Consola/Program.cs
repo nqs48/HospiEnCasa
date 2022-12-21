1170c9767b8dc342ec5cf673b92e8265a24a501e
Nestea Turner <nqs48@icloud.com>
2022-12-21 00:28:19 -0500

    Adding presentation layer <Consola>

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HospiEnCasa.App.Persistencia.AppRepositorios;
+using HospiEnCasa.App.Dominio.Entidades;
+
+namespace HospiEnCasa.App.Consola
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, I'm using Entity Framework!!");
+        }
+        
+    }
+}
